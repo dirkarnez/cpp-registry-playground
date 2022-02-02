f3d12acd4ba234299af777ea8fabcaad83766a9d
dirkarnez <smalldirkalex@gmail.com>
2022-02-02 08:14:24 +0800

    - update local-build-msvc2019.cmd

--- local-build-msvc2019.cmd
+++ local-build-msvc2019.cmd
@@ -9,7 +9,7 @@ set DOWNLOAD_DIR_LINUX=%DOWNLOAD_DIR:\=/%
 SET PATH=%DOWNLOAD_DIR%\PortableGit\bin;%DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;
 
 cmake -G "Visual Studio 16 2019" -A x64 ^
--DWINREG_ROOT="%~dp0../WinReg-v4.1.2-msvc2019" ^
+-DWINREG_ROOT="%~dp0../WinReg-v4.1.2-msvc2019/WinReg" ^
 -B./build
 
 cd build
